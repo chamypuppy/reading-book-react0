8350d508849de263dd1a0a18d05536292ea5bce0
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2024-07-23 14:06:36 +0900

    렌더링 최적화 실습
    재렌더링의 조건과 렌더링최적화 실습

---
+++ 3/src/App.jsx
@@ -0,0 +1,27 @@
+import { memo, useCallback, useState } from "react";
+import { Child1 } from "./components/Child1";
+import { Child4 } from "./components/Child4";
+
+export const App = memo(() => {
+    console.log("App 렌더링");
+
+    const [num, setNum] = useState(0);
+
+    const onClickButton = () => {
+        setNum(num+1);
+    };
+    
+    const onClickReset = useCallback(() => {
+        setNum(0);
+    }, []);
+
+    return (
+        <>
+            <button onClick={onClickButton}>버튼</button>
+            <p>{num}</p>
+            /* Props */
+            <Child1 onClickReset={onClickReset}/>
+            <Child4/>
+        </>
+    )
+});
---
+++ 3/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import { App } from "./App";
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+
+// index.js는 늘 이 3문장을 써야 
+// 리액트가 브라우저에 노출된다!
